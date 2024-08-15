4fe0d1893ffd19179ec4de6857318247629b976c
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2024-08-14 21:04:45 -0300

    OrderItem, OrderItemPK

--- src/main/java/com/tavodin/dscommerce/entities/Product.java
+++ src/main/java/com/tavodin/dscommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.tavodin.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -25,6 +26,9 @@ public class Product {
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -79,5 +83,13 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getProducts() {
+        return items.stream().map(x -> x.getOrder()).toList();
     }
 }
