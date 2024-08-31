fac69d4004107eadd8680be0a3eb5df74082a866
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2024-08-31 14:40:05 -0300

    Role, adding Role to User

--- src/main/java/com/tavodin/dscommerce/entities/User.java
+++ src/main/java/com/tavodin/dscommerce/entities/User.java
@@ -3,9 +3,7 @@ package com.tavodin.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -25,6 +23,12 @@ public class User {
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User() {}
 
@@ -87,6 +91,10 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
     }
 
     @Override
