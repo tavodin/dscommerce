5d73f8aa9ed818df2bef3c71bc7fc17638e8600e
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2024-09-04 14:06:23 -0300

    Fix validation

--- src/main/java/com/tavodin/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/tavodin/dscommerce/dto/ProductDTO.java
@@ -1,10 +1,7 @@
 package com.tavodin.dscommerce.dto;
 
 import com.tavodin.dscommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +18,7 @@ public class ProductDTO {
     @NotBlank(message = "Campo requerido")
     private String description;
 
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
--- src/main/java/com/tavodin/dscommerce/dto/ValidationError.java
+++ src/main/java/com/tavodin/dscommerce/dto/ValidationError.java
@@ -6,17 +6,18 @@ import java.util.List;
 
 public class ValidationError extends CustomError {
 
-    private List<FieldMessage> erros = new ArrayList<>();
+    private List<FieldMessage> errors = new ArrayList<>();
 
-    public ValidationError(Instant timestamp, Integer status, String error, String path) {
-        super(timestamp, status, error, path);
+    public ValidationError(Instant timestamp, Integer status, String errors, String path) {
+        super(timestamp, status, errors, path);
     }
 
-    public List<FieldMessage> getErros() {
-        return erros;
+    public List<FieldMessage> getErrors() {
+        return errors;
     }
 
     public void addError(String fieldName, String fieldMessage) {
-        erros.add(new FieldMessage(fieldName, fieldMessage));
+        errors.removeIf(x -> x.getField().equals(fieldName));
+        errors.add(new FieldMessage(fieldName, fieldMessage));
     }
 }
